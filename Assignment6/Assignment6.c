1159eca5993e48a0aedb0c443b8d024fa1c6bbbf
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2019-08-30 09:35:03 +0530

    Add files via upload

--- Assignment6/Assignment6.c
+++ Assignment6/Assignment6.c
@@ -7,46 +7,64 @@
 #define THINKING 0
 #define HUNGRY 1
 #define EATING 2
-#define LEFT (phil+4)%N
-#define RIGHT (phil+1)%N
-
+#define LEFT (phNum+N-1)%N
+#define RIGHT (phNum+1)%N
 sem_t mutex;
-sem_t S[5];
-
-void philosopher(int *);
-void pickup(int);
-void putdown(int);
-void test(int);
+sem_t S[N];
 
 int state[N];
 int phil[N] = {0,1,2,3,4};
+int eating[N] = {0};
 
-int main()
+void display()
 {
 	int i;
-	pthread_t thread_ID[N];
-	sem_init(&mutex,0,1);
+	printf("\n\n---------------------------------------------------");
+	printf("\nPHILOSOPHER NO:\t  ");
 	for(i=0; i<N; i++)
-		sem_init(&S[i],0,0);
-	
-	for(i=0; i<N; i++) {
-		pthread_create(&thread_ID[i],NULL,philosopher,&phil[i]);
-		printf("\nPHILOSOPHER %d IS THINKING!",i+1);
-	}
-
+		printf("%d\t",i+1);
+	printf("\n---------------------------------------------------");
+	printf("\nNO OF TIMES EATEN:");
 	for(i=0; i<N; i++)
-		pthread_join(thread_ID[i],NULL);
+		printf("%d\t",eating[i]);
+	printf("\n---------------------------------------------------");
+	printf("\n\n");
 }
 
-void test(int phil)
+void test(int phNum)
 {
-	if(state[phil] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING) {
-		state[phil] = EATING;
+	if(state[phNum] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING) {
+		state[phNum] = EATING;
+		eating[phNum]++;
 		sleep(2);
-		printf("\nPHILOSOPHER %d TAKES CHOPSTICK %d and %d",phil+1,LEFT+1,phil+1);
-		printf("\nPHILOSOPHER %d IS EATING!",phil+1);
-		sem_post(&S[phil]);
+		printf("\n\nPHILOSOPHER %d TAKES CHOPSTICK %d and %d",phNum+1,LEFT+1,phNum+1);
+		printf("\nPHILOSOPHER %d IS EATING!",phNum+1);
+		display();
+		sem_post(&S[phNum]);
 	}
+}
+
+void pickup(int phNum)
+{
+	sem_wait(&mutex);
+	state[phNum] = HUNGRY;
+	printf("\n\nPHILOSOPHER %d IS HUNGRY!",phNum+1);
+	test(phNum);
+	sem_post(&mutex);
+	sem_wait(&S[phNum]);
+	sleep(1);
+}
+
+void putdown(int phNum)
+{
+	sem_wait(&mutex);
+	state[phNum] = THINKING;
+	printf("\n\nPHILOSOPHER %d IS PUTTING DOWN THE CHOPSTICKS",phNum+1);
+	printf("\nCHOPSTICKS %d and %d ARE PUT DOWN!",LEFT+1,phNum+1);
+	printf("\nPHILOSOPHER %d IS THINKING!",phNum+1);
+	test(LEFT);
+	test(RIGHT);
+	sem_post(&mutex);
 }
 
 void *philosopher(void *num)
@@ -60,24 +78,19 @@ void *philosopher(void *num)
 	}
 }
 
-void pickup(int phil)
+int main()
 {
-	sem_wait(&mutex);
-	state[phil] = HUNGRY;
-	printf("\nPHILOSOPHER %d IS HUNGRY!",phil+1);
-	test(phil);
-	sem_post(&mutex);
-	sem_wait(&S[phil]);
-	sleep(1);
-}
+	int i;
+	pthread_t thread_ID[N];
+	sem_init(&mutex,0,1);
+	for(i=0; i<N; i++)
+		sem_init(&S[i],0,0);
+	
+	for(i=0; i<N; i++) {
+		pthread_create(&thread_ID[i],NULL,philosopher,&phil[i]);
+		printf("\n\nPHILOSOPHER %d IS THINKING!",i+1);
+	}
 
-void putdown(int phil)
-{
-	sem_wait(&mutex);
-	state[phil] = THINKING;
-	printf("\nPHILOSOPHER %d IS PUTTING DOWN THE CHOPSTICKS",phil+1);
-	printf("\nCHOPSTICKS %d and %d PUT DOWN!",LEFT+1,phil+1);
-	test(LEFT);
-	test(RIGHT);
-	sem_post(&mutex);
+	for(i=0; i<N; i++)
+		pthread_join(thread_ID[i],NULL);
 }
