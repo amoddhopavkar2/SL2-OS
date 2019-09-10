b13c5173243a3933f77bda66b798ba2ccfd5a74a
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2019-09-10 10:56:00 +0530

    Add files via upload

--- Assignment8/Assignment8c.c
+++ Assignment8/Assignment8c.c
@@ -9,14 +9,23 @@ int main()
 { 
     // ftok to generate unique key 
     key_t key = ftok("/home/oslab-22/33304/Assignment8",65); 
+  	if(key == -1) {
+    	printf("\nError while creating the key!\n");
+    	exit(0);
+    }
   
     // shmget returns an identifier in shmid 
-    int shmid = shmget(key,1024,0666|IPC_CREAT); 
+    int shmid = shmget(key,1024,0666|IPC_CREAT);
+    if(shmid < 0) {
+    	printf("\nError while creating the shared memory!\n");
+    	exit(0);
+    }; 
   
     // shmat to attach to shared memory 
     char *str = (char*) shmat(shmid,(void*)0,0); 
   
-    printf("Data read from memory: %s\n",str); 
+    printf("\nData read from memory: %s\n",str); 
+    printf("\nShared memory address: %X\n",shmid );
       
     //detach from shared memory  
     shmdt(str); 
--- Assignment8/Assignment8s.c
+++ Assignment8/Assignment8s.c
@@ -8,25 +8,31 @@
 int main() 
 {
     // ftok to generate unique key 
-    key_t key = ftok("/home/oslab-22/33304/Assignment8",65); 
+    key_t key = ftok("/home/oslab-22/33304/Assignment8",65);
+    if(key == -1) {
+    	printf("\nError while creating the key!\n");
+    	exit(0);
+    }
   
     // shmget returns an identifier in shmid 
-    int shmid = shmget(key,1024,0666|IPC_CREAT); 
+    int shmid = shmget(key,1024,0666|IPC_CREAT);
+    if(shmid < 0) {
+    	printf("\nError while creating the shared memory!\n");
+    	exit(0);
+    } 
   
     // shmat to attach to shared memory 
     char *str = (char*) shmat(shmid,(void*)0,0); 
   
     printf("\nEnter the string:");
-    gets(str); 
+    gets(str);
   
-    printf("Data written in memory: %s\n",str);
-
-    while(*str != '*') {
-    	sleep(1);
-    }
+    printf("\nData written in memory: %s\n",str);
+    printf("\nShared memory address: %X\n",shmid );
 
     //detach from shared memory  
     shmdt(str);
+   	while(1);
     
     return 0;
 } 
