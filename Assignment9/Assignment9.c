0ed9c8e17ecac60fd3bac4862f33925802d24ac9
Amod Dhopavkar <amoddhopavkar2@gmail.com>
2019-10-17 23:27:06 +0530

    Add files via upload

--- Assignment9/Assignment9.c
+++ Assignment9/Assignment9.c
@@ -4,14 +4,16 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-typedef struct student {
+typedef struct Student {
 	int roll;
 	char name[20];
 	float marks;
 	int class;
 }Student;
 
-void createData(char dataName[]) {
+char *dataName;
+
+void createData() {
 	int exists = access(dataName,F_OK);
 	if(exists == 0) 
 		printf("\nDATABASE ALREADY EXISTS!\n");
@@ -27,7 +29,7 @@ void createData(char dataName[]) {
 	}
 }
 
-void writeData(char dataName[]) {
+void writeData() {
 	Student s;
 	printf("\nENTER ROLL NO:");
 	scanf("%d",&s.roll);
@@ -50,7 +52,7 @@ void writeData(char dataName[]) {
 	close(fd);
 }
 
-void readData(char dataName[]) {
+void readData() {
 	Student s;
 	int fd = open(dataName,O_RDONLY);
 	if(fd < 0) {
@@ -65,7 +67,7 @@ void readData(char dataName[]) {
 	close(fd);
 }
 
-void searchData(char dataName[], int key) {
+void searchData(int key) {
 	Student s;
 	int flag, fd = open(dataName, O_RDONLY);
 	if(fd < 0) {
@@ -86,68 +88,64 @@ void searchData(char dataName[], int key) {
 	close(fd);
 }
 
-void deleteData(char dataName[], int key) {
+void deleteData(int key) {
 	char *tmp = "temp";
 	Student s;
 	int fd1 = open(dataName, O_RDONLY);
-	int fd2 = open(tmp, O_CREAT|O_WRONLY);
+	int fd2 = open(tmp, O_RDWR | O_CREAT, 0777);
 	if(fd1 < 0) {
 		printf("\nERROR IN READING THE FILE!");
 		exit(0);
 	}
-	else {
-		while(read(fd1,&s,sizeof(s))) {
-			if(s.roll == key) {
+	
+	while(read(fd1, &s, sizeof(s))) {
+		if(s.roll != key) {
+			write(fd2, &s, sizeof(s));
+		}
+		else {
 
-			}
-			else 
-				write(fd2,&s,sizeof(s));
 		}
 	}
-	remove(dataName);
-	rename(tmp,"dataName");
-
 	close(fd1);
 	close(fd2);
+	remove(dataName);
+	rename(tmp,dataName);
 }
 
-void modifyData(char dataName[], int key) {
+void modifyData(int key) {
 	char *tmp = "temp";
 	Student s1, s2;
 	int fd1 = open(dataName, O_RDONLY);
-	int fd2 = open(tmp, O_CREAT|O_WRONLY);
+	int fd2 = open(tmp, O_RDWR | O_CREAT, 0777);
 	if(fd1 < 0) {
 		printf("\nERROR IN READING THE FILE!");
 		exit(0);
 	}
-	else {
-		while(read(fd1, &s1, sizeof(s1))) {
-			if(s1.roll != key) 
-				write(fd2, &s1, sizeof(s1));
-			else {
-				printf("\nNEW ROLL NO:");
-				scanf("%d",&s2.roll);
-				printf("\nNEW NAME:");
-				scanf("%s",s2.name);
-				printf("\nNEW MARKS:");
-				scanf("%f",&s2.marks);
-				printf("\nNEW CLASS:");
-				scanf("%d",&s2.class);
-				write(fd2, &s2, sizeof(s2));
-			}
+	while(read(fd1, &s1, sizeof(s1))) {
+		if(s1.roll != key) 
+			write(fd2, &s1, sizeof(s1));
+		else {
+			printf("\nNEW ROLL NO:");
+			scanf("%d",&s2.roll);
+			printf("\nNEW NAME:");
+			scanf("%s",s2.name);
+			printf("\nNEW MARKS:");
+			scanf("%f",&s2.marks);
+			printf("\nNEW CLASS:");
+			scanf("%d",&s2.class);
+			write(fd2, &s2, sizeof(s2));
 		}
 	}
-	remove(dataName);
-	rename(tmp,dataName);
-
 	close(fd1);
 	close(fd2);
+	remove(dataName);
+	rename(tmp,dataName);
 }
 
 int main() {
 	int ch, key;
 	Student s;
-	char dataName[20];
+	char fileName[20];
 	while(1) {
 		printf("\n\n---STUDENTS DATABASE IN FILES---\n");
 		printf("\n1. CREATE DATABASE: \n2. INSERT RECORDS: \n3. DISPLAY RECORDS: \n4. SEARCH: \n5. DELETE: \n6. MODIFY: \n7. EXIT\n\nENTER YOUR CHOICE:");
@@ -155,34 +153,35 @@ int main() {
 		switch(ch) {
 			case 1:
 				printf("\nENTER DATABASE NAME:");
-				scanf("%s",dataName);
-				createData(dataName);
+				scanf("%s",fileName);
+				dataName = fileName;
+				createData();
 				break;
 
 			case 2:
-				writeData(dataName);
+				writeData();
 				break;
 
 			case 3:
-				readData(dataName);
+				readData();
 				break;
 
 			case 4:
 				printf("\nENTER A ROLL NO:");
 				scanf("%d",&key);
-				searchData(dataName,key);
+				searchData(key);
 				break;
 
 			case 5:
 				printf("\nENTER A ROLL NO:");
 				scanf("%d",&key);
-				deleteData(dataName,key);
+				deleteData(key);
 				break;
 
 			case 6:
 				printf("\nENTER A ROLL NO:");
 				scanf("%d",&key);
-				modifyData(dataName,key);
+				modifyData(key);
 				break;
 
 			case 7:
